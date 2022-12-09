90cdfdf5ca27b295f72989ff0116a4a80ebf0b98
JoseTomasSilvaZ <josetomassilvaz@gmail.com>
2022-12-08 23:46:35 -0300

    all working

--- src/Components/Statistics/MostFavoriteToken.jsx
+++ src/Components/Statistics/MostFavoriteToken.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
-const MostSoldToken = () => {
+const MostFavoriteToken = () => {
   const [loading, setLoading] = useState(true)
   const [tokenData, setTokenData] = useState()
   const fetchData  = async() => {
-    const data = await fetch('http://localhost:5000/api/querys/most-sold-token')
+    const data = await fetch('http://localhost:5000/api/querys/most-favorite-token')
     const result = await data.json()
     setTokenData(result[0])
     setLoading(false)
@@ -19,18 +19,19 @@ const MostSoldToken = () => {
     <div className='flex items-center '>
         <div className='w-1/2 flex flex-col items-center' >
 
-        <h1 className='text text-start text-white font-bold text-xl mb-3'>Token mas vendido</h1>
+        <h1 className='text text-start text-white font-bold text-xl mb-3'>Token con mas favoritos</h1>
         <div>
             <div>
             <img src={tokenData.url} className='w-[250px] h-[250px] object-cover rounded-md'/>
             <h1 className='text-md text-white font-bold mt-2 '>{tokenData.name}</h1>
-            <p className='text-sm'>🔥 {tokenData.count} veces vendido</p>
+            <p className='text-sm'>💖 {tokenData.fav_count} Likes</p>
             </div>
         </div>
         </div>
         <div className="mockup-code h-full w-1/2">
-  <pre data-prefix="$"><code>select * from tokens t join (select token_id, count(token_id) from sales group by token_id order by </code></pre> 
-  <pre data-prefix="$" className=""><code>count desc limit 1) as tmax on tmax.token_id = t.id; </code></pre> 
+  <pre data-prefix="$"><code>select * from tokens t join (select token_id,  </code></pre> 
+  <pre data-prefix="$" className=""><code>count(token_id) from sales group by token_id order by </code></pre> 
+  <pre data-prefix=">" className=""><code>count desc limit 1) as tmax on tmax.token_id = t.id;</code></pre>
 </div>
     </div>
     )}
@@ -38,4 +39,4 @@ const MostSoldToken = () => {
   )
 }
 
-export default MostSoldToken
+export default MostFavoriteToken
--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import MostFavoriteToken from '../Components/Statistics/MostFavoriteToken'
 import MostSoldToken from '../Components/Statistics/MostSoldToken'
 import ThreeUsersWithMoreMoney from '../Components/Statistics/ThreeUsersWithMoreMoney'
 import UserWithMoreTokens from '../Components/Statistics/UserWithMoreTokens'
 
 const Statistics = () => {
   return (
-    <div className='h-screen'>
+    <div className='w-full h-full'>
 
     <div className='flex flex-col h-full justify-evenly gap-12'>
         <MostSoldToken/>
         <UserWithMoreTokens/>
         <ThreeUsersWithMoreMoney/>
+        <MostFavoriteToken/>
         
     </div>
     </div>
